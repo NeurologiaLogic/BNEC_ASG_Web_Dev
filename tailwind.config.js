27073f15e925e06c0c3941667c1ec0eff185a9f7
patrick <patrickkwon777@gmail.com>
2023-05-20 11:40:57 +0700

    initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './src/**/*.{html,js}'
+  ],
+  theme: {
+    colors:{
+      "bnec-primary":"#496F24",
+      "bnec-black":"#F2F2F2",
+    },
+    extend: {},
+  },
+  plugins: [],
+}
+
